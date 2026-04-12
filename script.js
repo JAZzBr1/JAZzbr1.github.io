fc399ecbec050364596d61cbdaac05c3e2da38d9
JAZzBr1 <joaoheithor979@gmail.com>
2026-04-12 19:03:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function SayHi() {
     alert("Hello, Visitor");
 }
+function playMusic(id) {
+  document.getElementById(id).play();
+}
+
+function pauseMusic(id) {
+  document.getElementById(id).pause();
+}
